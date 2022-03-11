94d725d9e3f5c10070152b03ed251d06e6d1ce82
tulolo287 <iv1055@yandex.ru>
2022-03-11 16:32:40 +0300

    body def.

--- core/src/com/platformer/states/Play.java
+++ core/src/com/platformer/states/Play.java
@@ -1,14 +1,36 @@
 package com.platformer.states;
 
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.platformer.handlers.GameStateManager;
 
 public class Play extends GameState {
 
     private final BitmapFont font = new BitmapFont();
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
 
     public Play(GameStateManager gsm) {
         super(gsm);
+        world = new World(new Vector2(0, -9.81f), true);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set(50, 50);
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        Body body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(50, 10);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        body.createFixture(fixtureDef);
     }
 
     @Override
@@ -18,11 +40,12 @@ public class Play extends GameState {
 
     @Override
     public void update(float dt) {
-
+        world.step(dt, 6, 2);
     }
 
     @Override
     public void render() {
+        box2DDebugRenderer.render(world, camera.combined);
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
         font.draw(spriteBatch, "Hello", 100, 100);
